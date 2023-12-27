2a5121a04adfebc78e5cf8fe6709fd7d0fbfc930
migueel15 <miguelangeldorado10@gmail.com>
2023-12-27 01:17:47 +0100

    add: nvim-tree plugin

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,2 +1,2 @@
 require("toggleterm").setup {}
-vim.keymap.set({ "n", "t" }, "<C-p>", "<cmd>ToggleTerm<cr>", { noremap = true, silent = true })
+vim.keymap.set({ "n", "t" }, "<C-p>", "<cmd>ToggleTerm<cr>", { desc = "Toggle terminal", noremap = true, silent = true })
