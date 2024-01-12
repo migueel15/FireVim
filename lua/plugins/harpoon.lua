be6f22dd3a53b83fee9cd5772100770e5ed89a27
migueel15 <miguelangeldorado10@gmail.com>
2024-01-12 14:24:00 +0100

    add: bind for select current line with Tab

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -6,6 +6,11 @@ harpoon:setup()
 vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
 vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
+vim.keymap.set("n", "<Tab>", function()
+  local idx = vim.fn.line(".")
+  harpoon:list():select(idx)
+end)
+
 vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
 vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
 vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
